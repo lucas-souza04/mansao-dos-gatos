4c0e8bfe98c30f448d2ec5a8b35faa2f8744a87d
MiSilva95 <michelesilva344@gmail.com>
2024-09-14 13:52:27 -0300

    Desenvolvimento site ONG

---
+++ ajudar.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/logo.png" />
+    <title>Quero Ajudar - Mansão dos Cats</title>
+    <link rel="stylesheet" href="styles/ajudar.css">
+</head>
+
+<body>
+    <header>
+        <div class="top-bar">
+            <img src="assets/logo.png" alt="Logo" class="logo">
+            <h2><a href="index.php">www.mansaodoscats.org.br</a></h2>
+            <div class="social-icons">
+                <a href="https://www.instagram.com/mansaodoscats/" target="_blank"><img src="assets/instagram.png"
+                        alt="Instagram"></a>
+                <a href="https://www.facebook.com/mansaodoscats?locale=pt_BR" target="_blank"><img
+                        src="assets/facebook.png" alt="Facebook"></a>
+                <a href="https://wa.me" target="_blank"><img src="assets/whatsapp.png" alt="WhatsApp"></a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <h1>Ajude os Gatinhos da Mansão dos Cats!</h1><br><br>
+            <p>Quando cuidávamos de um punhadinho de gatos, já tínhamos dificuldade, mas nós dávamos conta de bancar com
+                dinheiro do nosso próprio bolso as vacinas, castrações, tratamentos e alimentação dos gatinhos.
+                Aos poucos, as pessoas foram se interessando pelo nosso trabalho e começaram a nos ajudar. Fomos tirando
+                mais e mais gatinhos das ruas. Hoje, todo o trabalho da Mansão dos Cats não seria possível sem as
+                doações e sem a ajuda de pessoas maravilhosas que contribuem regular ou esporadicamente com o nosso
+                trabalho.
+                Temos medo que o dinheiro falte e que nossos gatinhos passem por dificuldades, até porque jamais
+                daríamos
+                conta dessa turma toda sozinhos. E é por isso que as doações em ração, dinheiro e produtos são
+                essenciais à manutenção e continuidade do nosso trabalho. Afinal, ainda não contamos com patrocínio de
+                nenhuma empresa.</p>
+            <br>
+            <h1>Doações em Dinheiro</h1><br>
+            <p>O dinheiro que recebemos é usado para pagarmos por cirurgias (inclusive as de castração), tratamentos,
+                internações, compra de vacinas e de tudo o que os gatinhos precisam. Consideramos o tipo de doação mais
+                importante!</p>
+            <br>
+            <h1>Pix dos Gatinhos</h1><br>
+            <div class="pix-image">
+                <h2>Pix Celular: (11) 98259-0004</h2><br>
+                <img src="assets/pix.png">
+            </div>
+            <br>
+            <h1>Doações em Produtos</h1><br>
+            <p>Aceitamos doações de ração, areia, remédios, caminhas, arranhadores, caixas de transporte e cobertores.
+                Remédios veterinários: Enropet, Baytril, etc.Remédios humanos: Cobavital, Metronidazol, etc.
+                Descartáveis: Seringas, luvas, esparadrapo, etc. Papelaria e limpeza: Papel sulfite, álcool 70%,
+                etc..<br>Para saber como entregar sua doação pessoalmente, entrar em contato conosco pelo <a
+                    href="https://wa.me/5511982590004">Whatsapp.</a></p>
+            <br>
+            <h1>Apadrinhando um Gatinho</h1><br>
+            <p>Seja madrinha ou padrinho dos nossos gatinhos. Você faz uma contribuição fixa mensal e ajuda a pagar
+                pelas despesas do gatinho escolhido.<br>Para saber sobre como apadrinhar um gatinho entre em contato
+                conosco pelo <a href="https://wa.me/5511982590004">Whatsapp.</a></p>
+        </div>
+        <section class="info">
+            <div class="top-section">
+                <div class="text-section">
+                    <h2>Mansão dos Cats</h2>
+                    <div class="description">
+                        <p>A Mansão dos Cats busca lares para gatos encontrados abandonados na Grande São Paulo<br>e
+                            também trabalha para conscientizar as pessoas sobre a importância da castração e
+                            posse<br>responsável. Também, resgata, trata, castra e doa gatinhos para lares seguros em
+                            São Paulo.</p>
+                    </div>
+                </div>
+                <div class="menu-section">
+                    <h3>Menu</h3>
+                    <div class="menu-buttons">
+                        <a href="ong.php" class="menu-button">A ONG</a>
+                        <a href="adotar.php" class="menu-button">Quero adotar</a>
+                        <a href="ajudar.php" class="menu-button">Quero ajudar</a>
+                    </div>
+                </div>
+            </div>
+            <p class="copyright"><img src="assets/copyright.png" alt="Copyright">2024 - Mansão dos Cats. Todos os
+                direitos reservados.</p>
+        </section>
+    </main>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/logo.png"/>
+    <title>Mansão dos Cats</title>
+    <link rel="stylesheet" href="styles/index.css">
+</head>
+
+<body>
+    <header>
+        <div class="top-bar">
+            <img src="assets/logo.png" alt="Logo" class="logo">
+            <h2>www.mansaodoscats.org.br</h2>
+            <div class="social-icons">
+                <a href="https://www.instagram.com/mansaodoscats/" target="_blank"><img src="assets/instagram.png"></a>
+                <a href="https://www.facebook.com/mansaodoscats?locale=pt_BR" target="_blank"><img src="assets/facebook.png"></a>
+                <a href="https://wa.me/5511982590004" target="_blank"><img src="assets/whatsapp.png"></a>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <section class="content">
+            <div class="cat-image">
+                <img src="assets/gatos.png">
+            </div>
+            <div class="buttons">
+                <a class="button" href="adotar.php">
+                    <img src="assets/pata.png">
+                    <p>QUERO ADOTAR</p>
+                </a>
+                <a class="button" href="ajudar.php">
+                    <img src="assets/coracao.png">
+                    <p>QUERO AJUDAR</p>
+                </a>
+                <a class="button" href="ong.php">
+                    <img src="assets/mansao.png" alt="Logo da Mansão dos Gatos">
+                    <p>A ONG</p>
+                </a>
+
+            </div>
+        </section>
+        <section class="info">
+            <div class="top-section">
+                <div class="text-section">
+                    <h2>Mansão dos Cats</h2>
+                    <div class="description">
+                        <p>A Mansão dos Cats busca lares para gatos encontrados abandonados na Grande São Paulo<br>e
+                            também trabalha para conscientizar as pessoas sobre a importância da castração e
+                            posse<br>responsável. Também, resgata, trata, castra e doa gatinhos para lares seguros em
+                            São Paulo.</p>
+                    </div>
+                </div>
+                <div class="menu-section">
+                    <h3>Menu</h3>
+                    <div class="menu-buttons">
+                        <a href="ong.php" class="menu-button">A ONG</a>
+                        <a href="adotar.php" class="menu-button">Quero adotar</a>
+                        <a href="ajudar.php" class="menu-button">Quero ajudar</a>
+                    </div>
+                </div>
+            </div>
+            <p class="copyright"><img src="assets/copyright.png" alt="Copyright">2024 - Mansão dos Cats. Todos os
+                direitos reservados.</p>
+        </section>
+    </main>
+    <script src="script.js"></script>
+</body>
+
+</html>
